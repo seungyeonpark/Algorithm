d576d3490f62387e5ec593a0a1586cff070354cd
seungyeonpark <siaayeon@gmail.com>
2022-01-04 22:58:51 +0900

    code : stack with c

---
+++ Datastructure/Stack/StackWithC.c
@@ -0,0 +1,40 @@
+enum { MAX_NUMS = 8 };
+
+int s_nums[MAX_NUMS];
+size_t s_num_count = 0;
+
+void push(int n);
+int is_empty(void);
+int pop(void);
+
+int main(void)
+{
+    int num;
+
+    push(88);
+    push(44);
+    push(22);
+
+    num = pop();
+    num = pop();
+
+    return 0;
+}
+
+void push(int n)
+{
+    assert(s_num_count < MAX_NUMS);
+    s_nums[s_nums_count++] = n;
+}
+
+int is_empty(void)
+{
+    return (s_num_count == 0);
+}
+
+int pop(void)
+{
+    assert(is_empty == 0); // 0 == false
+
+    return s_nums[--s_num_count];
+}
