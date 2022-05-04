c2eb4d873b22945c7bf2a5a6aca38f269c338665
seungyeonpark <siaayeon@gmail.com>
2022-05-04 23:13:32 +0900

    boj 5622 다이얼

---
+++ String/BOJ_5622.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ('A' <= c && c <= 'C') {
+                sum += 3;
+            } else if ('D' <= c && c <= 'F') {
+                sum += 4;
+            } else if ('G' <= c && c <= 'I') {
+                sum += 5;
+            } else if ('J' <= c && c <= 'L') {
+                sum += 6;
+            } else if ('M' <= c && c <= 'O') {
+                sum += 7;
+            } else if ('P' <= c && c <= 'S') {
+                sum += 8;
+            } else if ('T' <= c && c <= 'V') {
+                sum += 9;
+            } else if ('W' <= c && c <= 'Z') {
+                sum += 10;
+            }
+        }
+        System.out.println(sum);
+	}
+}
