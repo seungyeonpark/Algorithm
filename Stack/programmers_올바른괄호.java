aeedad3ccad283acc789928d04dbca06c34feb99
seungyeonpark <siaayeon@gmail.com>
2022-10-24 22:00:04 +0900

    stack

---
+++ Stack/programmers_올바른괄호.java
@@ -0,0 +1,19 @@
+class Solution {
+    boolean solution(String s) {
+        
+        int cnt = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '(') {
+                ++cnt;
+            } else {
+                --cnt;
+            }
+            
+            if (cnt < 0) {
+                break;
+            }
+        }
+        
+        return cnt == 0 ? true : false;
+    }
+}
